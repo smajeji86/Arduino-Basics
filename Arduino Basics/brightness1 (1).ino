34dfaf66d0da3e6ff37b7991ca6405d8c2908759
Shrivats Majeji <smajeji86@gmail.com>
2021-02-02 23:58:20 +0530

    first commit

---
+++ Arduino Basics/brightness1 (1).ino
@@ -0,0 +1,36 @@
+int switchPin = 8;
+int ledPin = 11;
+boolean lastButton = LOW;
+boolean currentButton = LOW;
+int ledLevel = 0;
+ 
+void setup()
+{
+  pinMode(switchPin,INPUT);
+  pinMode(ledPin,OUTPUT);
+}
+
+boolean debounce(boolean last)
+{
+  boolean current = digitalRead(switchPin);
+  if (last != current)
+  {
+    delay(5);
+    current = digitalRead(switchPin);
+  }
+  return current;
+}
+
+void loop()
+{
+  currentButton = debounce(lastButton);
+  if (currentButton == HIGH && lastButton == LOW)
+  {
+    ledLevel = ledLevel + 51;
+  }
+
+  lastButton = currentButton;
+  
+  if (ledLevel > 255) ledLevel = 0;
+  analogWrite(ledPin,ledLevel);
+}
